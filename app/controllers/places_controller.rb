ff59794573bafe4138accf7192b48f1ee9325fda
Joshua Bangle <joshua.bangle@gmail.com>
2019-07-31 03:23:23 +0000

    Added an edit button to places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -17,6 +17,16 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
   end
 
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to place_path(params[:id]) #I wanted it to link back to the edited place, not to the full index. How else will the user see if their edit went through easily?
+  end
+
   private
 
   def place_params
