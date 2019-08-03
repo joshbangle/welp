efc55fa4ede1a427e421fd50a8460f0821d0e406
Joshua Bangle <joshua.bangle@gmail.com>
2019-08-03 20:35:33 +0000

    added geolocation

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,10 @@ class Place < ApplicationRecord
     paginates_per 5
 
     belongs_to :user
+    geocoded_by :address
+    after_validation :geocode
+    
     validates :name, :address, :description, presence: true
+
   
 end
