7af3dabc1b439d71c65976f7798b0b90923efdfa
Joshua Bangle <joshua.bangle@gmail.com>
2019-08-03 22:36:04 +0000

    added database info for comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,10 +2,13 @@ class Place < ApplicationRecord
     paginates_per 5
 
     belongs_to :user
+    has_many :comments
+
+    
     geocoded_by :address
     after_validation :geocode
     
     validates :name, :address, :description, presence: true
 
-  
+    
 end
